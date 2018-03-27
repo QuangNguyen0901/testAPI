9945af73e8a31f795d457b3988a301fbb33c959e
Minh Quang <hikari0901@gmail.com>
2018-03-27 04:43:22 +0000

    linode

--- libs/config.php
+++ libs/config.php
@@ -1,8 +1,9 @@
 <?php
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','');
-define('DB_DBNAME','qblog');
-define('HOST','qblog.com');
+define('DB_PASS','Ln123456');
+define('DB_DBNAME','testapi');
+
+define('HOST','mrqtest.tk');
 
 $root = $_SERVER['DOCUMENT_ROOT'];
