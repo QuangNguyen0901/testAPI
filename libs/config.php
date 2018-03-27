021d9eace2890c0105cb2fd9999b34cb6d5a5eeb
QuangNguyen <hikari0901@gmail.com>
2018-03-27 11:13:39 +0700

    aa

---
+++ libs/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_DBNAME','qblog');
+define('HOST','qblog.com');
+
+$root = $_SERVER['DOCUMENT_ROOT'];
+
+//aaaabbbbccccdddd
