76d1cfc04f9f935a11d0a4b62da7637c1c21a608
QuangNguyen <hikari0901@gmail.com>
2018-03-27 11:40:20 +0700

    aaaa

--- libs/connection.php
+++ libs/connection.php
@@ -1,9 +1,9 @@
 <?php
-
-$db_user = 'root'; //User đăng nhập MYSQL
-$db_pass = ''; // Pass đăng nhập MySQL
-$db_host = 'localhost'; //IP, Domain kết nối
-$db_name = 'qblog'; //Tên CSDL
+include('../libs/config.php');
+$db_user = DB_USER; //User đăng nhập MYSQL
+$db_pass = DB_PASS; // Pass đăng nhập MySQL
+$db_host = DB_HOST; //IP, Domain kết nối
+$db_name = DB_DBNAME; //Tên CSDL
 //Tạo biến kết nối với CSDL
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die('Kết nối thất bại');
 ?>
